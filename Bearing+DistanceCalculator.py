77ff451b675289c94a48f93c4199a50b27acb73e
AlexStad <42697745+AlexStad@users.noreply.github.com>
2018-08-26 19:28:47 +0200

    Update Bearing+DistanceCalculator.py
    Put everything in a single function

--- Bearing+DistanceCalculator.py
+++ Bearing+DistanceCalculator.py
@@ -3,7 +3,7 @@
 
 import math
 
-#Bearing Calculation:
+#Coordinates Input:
 
 x1 = math.radians(float(input("Enter the X coordinates of Point 1: ")))
 y1 = math.radians(float(input("Enter the y coordinates of Point 1: ")))
@@ -11,16 +11,18 @@ x2 = math.radians(float(input("Enter the x coordinates of Point 2: ")))
 y2 = math.radians(float(input("Enter the y coordinates of Point 2: ")))
 Deltax = abs(abs(x1)-abs(x2))
 Deltay = abs(abs(y1)-abs(y2))
-x = math.cos(x2) * math.sin(Deltay)
-y = math.cos(x1) * math.sin(x2) - math.sin(x1) * math.cos(x2) * math.cos(abs(abs(y1)-abs(y2)))
-bearing = (math.degrees(math.atan2(x, y)) + 360) % 360
 
-print(bearing)
+#Bearing and Distance Calculation:
 
-#Distance Calculation (Haversine Formula)
+def Calc(x1, y1, x2, y2, Deltax, Deltay):
+    x = math.cos(x2) * math.sin(Deltay)
+    y = math.cos(x1) * math.sin(x2) - math.sin(x1) * math.cos(x2) * math.cos(abs(abs(y1)-abs(y2)))
+    bearing = (math.degrees(math.atan2(x, y)) + 360) % 360
+    a = math.pow(math.sin(Deltax/2), 2) + math.cos(x1) * math.cos(x2) * math.pow(math.sin(Deltay / 2), 2) 
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = 6371 * c
+    calc = (bearing, distance)
+    return calc
 
-a = math.pow(math.sin(Deltax/2), 2) + math.cos(x1) * math.cos(x2) * math.pow(math.sin(Deltay / 2), 2) 
-c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-distance = 6371 * c
-print(distance)
-
+print(Calc(x1, y1, x2, y2, Deltax, Deltay)[0])
+print(Calc(x1, y1, x2, y2, Deltax, Deltay)[1])
