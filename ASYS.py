3ddd5be2135c21fefcbcb5af942398d7d1c70aa4
Alexander Stadelmann <42697745+AlexStad@users.noreply.github.com>
2018-09-05 12:23:11 -0900

    Merged all existing scripts
    Also changed from gpsd to direct serial parsing with the GPS.

--- ASYS.py
+++ ASYS.py
@@ -1,3 +1,92 @@
 #Autonomous System (ASYS)
 #SMSRecieve + GPSLocationData + Bearing+DistanceCalculator
 #Made for Python 2.7 due to legacy gpsd module
+
+import serial
+import time
+import math
+
+ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
+
+def SerialCOM(text):                #Communicates Commands
+    text = text + "\r\n"
+    ser.write(text)
+    data = ""
+    time.sleep(2)
+    while ser.inWaiting() != 0:
+        while ser.inWaiting() > 0:
+            data += ser.read(ser.inWaiting())
+    return data
+
+def GPSInfo(bracket):                                   #Fetches GPS info
+    while "OK" not in SerialCOM("AT+CGNSINF"):              #GPS COM Check
+        time.sleep(20)
+    while "0" in SerialCOM("AT+CGNSINF").split(",")[1]:     #GPS FIX Check
+        time.sleep(20)
+    info = SerialCOM("AT+CGNSINF").split(",")[bracket]      #Parsing info
+    return info
+
+def Calc():               #Calculates Bearing and Distance
+    x1 = math.radians(float(GPSInfo(3)))    #fetches longitude from GPS
+    y1 = math.radians(float(GPSInfo(4)))    #same, but latitude
+    x2 = math.radians(float(SMSParser()[0]))
+    y2 = math.radians(float(SMSParser()[1]))
+    Deltax = abs(abs(x1)-abs(x2))
+    Deltay = abs(abs(y1)-abs(y2))
+    x = math.cos(x2) * math.sin(Deltay)
+    y = math.cos(x1) * math.sin(x2) - math.sin(x1) * math.cos(x2) * math.cos(abs(abs(y1)-abs(y2)))
+    bearing = (math.degrees(math.atan2(x, y)) + 360) % 360
+    a = math.pow(math.sin(Deltax/2), 2) + math.cos(x1) * math.cos(x2) * math.pow(math.sin(Deltay / 2), 2) 
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = 6371 * c
+    calc = (bearing, distance)
+    return calc
+
+def SMSCheck():
+    while True:
+        SMS = SerialCOM("AT+CMGR=1")
+        if "UNREAD" in SMS:
+            print "Unread SMS arrived"
+            if "Help me" in SMS:
+                print "SMS is from tracker" 
+                return SMS
+                break
+            else:
+                print "SMS is not from tracker, aborting!" 
+        time.sleep(20)
+
+def SMSParser():
+    print"SMS"
+    message = SMSCheck().split(",")
+    print(message)
+    xmessage = message[5]
+    ymessage = message[7]
+    xmessage1 = xmessage[0:1]
+    xmessage2 = xmessage[2:6]
+    xmessage2 = float(xmessage2)/60
+    x2= float(xmessage1 + "." + xmessage2)
+    print(x2)
+    ymessage1 = ymessage[0:1]
+    ymessage2 = ymessage[2:6]
+    ymessage2 = float(ymessage2)/60
+    y2= float(ymessage1 + "." + ymessage2)
+    print(y2)
+    coordinates = (x2, y2)
+    return coordinates
+
+#Prelaunch Checks
+
+while "OK" not in SerialCOM("AT"):                  #General COM Check
+    time.sleep(20)
+
+while "OK" not in SerialCOM("AT+CGNSPWR=1"):        #GPS Power Activation
+    time.sleep(20)
+
+#while "READY" not in SerialCOM("AT+CPOM?"):         #SIM/GSM Check
+   # time.sleep(20)
+
+#Launch
+print"start"
+print Calc()
+print GPSInfo(7)
+
