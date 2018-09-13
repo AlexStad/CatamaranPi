5f622a885c4dcd84c344732e027068c3c5195962
Alexander Stadelmann <42697745+AlexStad@users.noreply.github.com>
2018-09-14 01:54:02 +0200

    Routing and Overhaul
    Routing added and large overhaul of start sequence, stand-by and RTB

--- ASYS.py
+++ ASYS.py
@@ -8,18 +8,85 @@ import math
 
 
 
-ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
-Coords = ""
+ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)    #Serial Set-up
+
+Coords = ""                     #Variable setting
+TurnCoords = ""
+SMSCoords = ""
 ObjectiveDistance = 42
+
+X1 = round(47.129780, 4)    #Routing line coordinates
+Y1 = round(8.781155, 4)     #Only in middle of Sihlsee!
+X2 = round(47.101284, 4)
+Y2 = round(8.796171, 4)
+DeltaX = X2-X1              #GPSLine List Calculations
+DeltaY = Y2-Y1
+DivX = -0.0001
+DivY = (DeltaY/DeltaX) * DivX
+GPSXList = []
+GPSYList = []
+X = X1
+Y = Y1
+count = 0
+while count != 536:
+    X = round(X1+(DivX*count), 4)
+    Y = round(Y1+(DivY*count), 4)
+    GPSXList.append(X)
+    GPSYList.append(Y)
+    count += 1
+
 
 class ASYS:
 
     def __init__(self):
-        print("Starting.")
+        print("Autonomous System initiated.\nStarting Prelaunch Checks.")
         ASYS.PrelaunchChecks(self)
-        print("Prelaunch Checks completed.")
+        print("Prelaunch Checks completed")
+        print("Setting Stand-By Position coordinates.")
+        ASYS.StandBy(self)
+        print("Arrived at Stand-By position.\nStarting Mission.")
+        while True:
+            ASYS.Mission(self)
+            "Checking for RTB command."
+            if ASYS.RTBCheck(self):
+                break
+            else:
+                print("No RTB command recieved")
+        print("Setting Base coordinates.")
+        ASYS.RTB(self)
+
+
+
+
+    def Mission(self):
+        global Coords
+        global SMSCoords
+        print("Mission started.")
         ASYS.SMS(self)
-        print("SMS parsed.\nInitiating start up sequence.\n")
+        print("SMS parsed.\nRouting.\n")
+        ASYS.Routing(self, SMSCoords[0], SMSCoords[1])
+        Coords = TurnCoords
+        print("GPS Routed.\nInitiating approach to turn point.")
+        ASYS.EXEC(self)
+        print("Turn point reached. Setting final approach coordinates.")
+        Coords = SMSCoords
+        print("Final approach coordinates set.\nInitiating final approach.")
+        ASYS.EXEC(self)
+        print("Arrived at destination. Entering sleep mode.")
+        input("Press enter to exit sleep mode.")
+        print("Sleep mode exit initiated.")
+        print("2 Minutes until Return to Stand-By Position.")
+        time.sleep(120)
+        Coords = TurnCoords
+        print("GPS Routed.\nInitiating approach to turn point.")
+        ASYS.EXEC(self)
+        print("Turn point reached.\nSetting Stand-By Position coordinates.")
+        ASYS.StandBy(self)
+        print("Arrived at Stand-By position.\nRestarting Mission.")
+
+
+
+    def EXEC(self):
         while ObjectiveDistance > 0.01:
             try:
                 Calc = ASYS.Calc(self)
@@ -30,6 +97,7 @@ class ASYS:
             print("Bearing Delta: \t", Calc[0], "deg")
             print("Distance: \t", Calc[1], "km\n")
             time.sleep(20)
+
 
 
     def PrelaunchChecks(self):
@@ -94,8 +162,11 @@ class ASYS:
         return calc
 
 
-    def SMSCheck(self):
+    def Watch(self):
+        print("Watch mode set.")
+        count = 0
         while True:
+            count += 1
             SMS = ASYS.SerialCOM(self, "AT+CMGL=\"REC UNREAD\"")
             IDText = "Help Me"
             if IDText in SMS:
@@ -103,12 +174,16 @@ class ASYS:
                 SMS = SMS[SMS.find(IDText):len(SMS)]
                 return SMS
                 break
+            if count = 31:          #Returns to Stand By Position every 10 min
+                ASYS.StandBy(self)
+                count = 0
             time.sleep(20)
 
 
+
     def SMS(self):
-        print("SMS Reception activated.")
-        message = ASYS.SMSCheck(self).split(",")
+        print("Setting Watch mode.")
+        message = ASYS.Watch(self).split(",")
         xmessage = message[5]
         ymessage = message[7]
         xmessage1 = xmessage[:-7]
@@ -121,41 +196,49 @@ class ASYS:
         ymessage2 = float(ymessage2)/60
         y2 = (float(ymessage1) + float(ymessage2))
         print("SMS Y Coordinates: ", y2)
-        global Coords
-        Coords = (float(x2), float(y2))
-        return Coords
-    
-    
+        global SMSCoords
+        SMSCoords = (float(x2), float(y2))
+        return SMSCoords
+
+    def Routing(self, X1, Y1):
+        X1 = round(X1, 4)
+        Y1 = round(Y1, 4)
+        global GPSXList
+        global GPSYList
+        if X1 in GPSXList:
+            global TurnCoords
+            TurnCoords = (X1, GPSYList[GPSXList.index(X1)])
+            return TurnCoords
+        else:
+            print("ERROR: Goal Coordinates not in operation Range!")
+
+
+    def RTBCheck(self):
+        Command = False
+        SMS = ASYS.SerialCOM(self, "AT+CMGL=\"REC UNREAD\"")
+        IDText = "RTB!"
+        if IDText in SMS:
+            print("RTB Command received.")
+            Command = True
+        return Command
+
+
     def RTB(self):                     # Return to Base
         global Coords
         Coords = (47.123442, 8.775506) # coordinates of the catamaran's base
-        while ObjectiveDistance > 0.01:
-            try:
-                Calc = ASYS.Calc(self)
-            except:
-                print("ERROR: Distance and Bearing calculation interrupted.")
-                print("Initiating Emergency Stop.")
-                print("Attemting to restart.")
-            print("Bearing Delta: \t", Calc[0], "deg")
-            print("Distance: \t", Calc[1], "km\n")
-            time.sleep(20)
+        print("Base coordinates set.\nInitiating approach.")
+        ASYS.EXEC(self)
+        print("Catamaran arrived at base.")
+        print("Autonomous System shutting down.")
 
 
     def StandBy(self):                 # StandBy at the middle of the lake
         global Coords
-        Coords = (47.120697, 8.786875) # coordinates of the middle of the lake 
-        while ObjectiveDistance > 0.01:
-            try:
-                Calc = ASYS.Calc(self)
-            except:
-                print("ERROR: Distance and Bearing calculation interrupted.")
-                print("Initiating Emergency Stop.")
-                print("Attemting to restart.")
-            print("Bearing Delta: \t", Calc[0], "deg")
-            print("Distance: \t", Calc[1], "km\n")
-            time.sleep(20)
+        Coords = (47.120697, 8.786875) # coordinates of the middle of the lake
+        print("Stand-By coordinates set.\nInitiating approach.")
+        ASYS.EXEC(self)
 
-            
+
 #Launch
 
 ASYS()
